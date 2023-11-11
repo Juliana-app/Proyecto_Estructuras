6e875b4977881a2e7987e1e6682ba0b6431fbbc8
Juliana-app <julianaordonez002@gmail.com>
2023-11-10 23:23:59 -0500

    Nodos listos

--- Nodo.java
+++ Nodo.java
@@ -1,3 +1,11 @@
 public class Nodo {
-    
+    public int dato;
+    public Nodo anterior;
+    public Nodo siguiente;
+
+    public Nodo(int dato) {
+        this.dato = dato;
+        this.siguiente = null;
+        this.anterior = null;
+    }
 }
